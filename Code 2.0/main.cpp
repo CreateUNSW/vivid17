48d5441bfb7b7cac2cc5f5bdeb02e33aa36f7339
Jabez Wilson <jabez.wilson0@gmail.com>
2017-02-03 00:55:19 +1100

    added all necassary files and docs to generate edge configurations from the gh script

--- Code 2.0/main.cpp
+++ Code 2.0/main.cpp
@@ -69,37 +69,15 @@ void draw(int *d);
 // }
 
 int main(int argc, char ** argv) {
-    int edges[MAX_CRYSTALS_NUM][MAX_EDGE_PER_CRYSTAL + 1] = {{-1},
-                                                            {3, 4, 2, 7, 6, 5, 16, 15, 18, -1},
-                                                            {4, 9, 8, 7, 6, 1, 3, -1},
-                                                            {12, 11, 4, 2, 1, 16, 15, 18, 13, -1},
-                                                            {11, 10, 9, 2, 1, 3, 12, -1},
-                                                            {1, 6, 16, -1},
-                                                            {1, 2, 7, 5, -1},
-                                                            {2, 9, 8, 6, 1, -1},
-                                                            {9, 7, 2, -1},
-                                                            {10, 8, 7, 2, 4, 11, -1},
-                                                            {9, 4, 11, -1},
-                                                            {10, 9, 4, 3, 12, -1},
-                                                            {11, 4, 3, 18, 13, -1},
-                                                            {12, 3, 18, 17, -1},
-                                                            {16, 5, -1},
-                                                            {18, 3, 1, 16, -1},
-                                                            {15, 18, 3, 1, 6, 5, 14, -1},
-                                                            {13, 18, -1},
-                                                            {17, 13, 12, 3, 1, 16, 15, -1}};
+    int edges[MAX_CRYSTALS_NUM][MAX_EDGE_PER_CRYSTAL + 1] = {{-1}, {7, 4, 2, 18, 15, 3, 16, 6, 5, -1}, {8, 9, 7, 6, 4, 3, 1, -1}, {11, 13, 12, 4, 2, 18, 16, 15, 1, -1}, {10, 12, 11, 9, 3, 2, 1, -1}, {14, 16, 6, 1, -1}, {7, 2, 16, 5, 1, -1}, {9, 8, 6, 2, 1, -1}, {9, 7, 2, -1}, {11, 10, 8, 7, 4, 2, -1}, {11, 9, 4, -1}, {10, 9, 12, 4, 3, -1}, {11, 4, 18, 13, 3, -1}, {17, 18, 12, 3, -1}, {16, 5, -1}, {18, 16, 3, 1, -1}, {14, 18, 15, 3, 6, 5, 1, -1}, {18, 13, -1}, {17, 13, 12, 16, 15, 3, 1, -1}};
 
     Graph g(edges,19);
     int scanfFlag = 1;
-    int i = 0;
     int src = 0;
-    int *dist = NULL;
     int *d = NULL;
     
     // Recalculates distance
     d = g.calcDist(src);
-    // Clears the screen
-    system("clear");
     // Draws the mosaic
     draw(d);
 
@@ -122,10 +100,10 @@ int main(int argc, char ** argv) {
 
        delete[] d;
     }
-    printf("\n");
+
 }
 
-void p(int *dist, int i, char *panel, int space) {
+void p(int *dist, int i, const char *panel, int space) {
 
    int len = strlen(panel) / 3;
    int j = 0;
@@ -154,6 +132,10 @@ void p(int *dist, int i, char *panel, int space) {
 
 void draw(int *d) {
 
+    // for(int i=0; i<MAX_CRYSTALS_NUM; i++) {
+    //     cout << d[i] << " ";
+    // }
+    // cout <<endl;
             p(d,10,"██████████",2);                    p(d,9,"███████████████",3);                     p(d,8,"██████████",0);
 printf("  "); p(d,10,"███████",2);                    p(d,9,"█████████████████",3);                     p(d,8,"███████",0);   
 p(d,11,"█",3);  p(d,10,"███",3);                     p(d,9,"███████████████████",3);                     p(d,8,"███",4);p(d,7,"█",0);
