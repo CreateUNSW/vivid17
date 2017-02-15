881ffa94c8541f148eae574b125804da5d9b61b3
RandyTjang <randy.tjang@gmail.com>
2017-02-15 17:07:32 +1100

    Completed multi strip code

--- multiple_strips/multiple_strips.ino
+++ multiple_strips/multiple_strips.ino
@@ -22,45 +22,47 @@ int lookup[MAX_CRYSTALS_NUM] =
 {0,15,18,13,12,3,4,11,10,16,5,6,7,8,9,2,1};
 //,5,6,7,8,9,2,1
 Graph * g;
-int j = 15;
-int k = 0;
+int *dist;
+int blue = 0;
+int blueFlag = 0;
+
 void setup() {
-      FastLED.addLeds<LED_TYPE, DATA_PIN0, RGB> (C, 0, LEDS_PER_PIN);
-      int edges[MAX_CRYSTALS_NUM][MAX_EDGE_PER_CRYSTAL + 1] = {{-1}, {7, 4, 2, 18, 15, 3, 16, 6, 5, -1}, {8, 9, 7, 6, 4, 3, 1, -1}, {11, 13, 12, 4, 2, 18, 16, 15, 1, -1}, {10, 12, 11, 9, 3, 2, 1, -1}, {14, 16, 6, 1, -1}, {7, 2, 16, 5, 1, -1}, {9, 8, 6, 2, 1, -1}, {9, 7, 2, -1}, {11, 10, 8, 7, 4, 2, -1}, {11, 9, 4, -1}, {10, 9, 12, 4, 3, -1}, {11, 4, 18, 13, 3, -1}, {17, 18, 12, 3, -1}, {16, 5, -1}, {18, 16, 3, 1, -1}, {14, 18, 15, 3, 6, 5, 1, -1}, {18, 13, -1}, {17, 13, 12, 16, 15, 3, 1, -1}};
-      g = new Graph(edges,MAX_CRYSTALS_NUM);
-      Serial.begin(9600);
-      FastLED.addLeds<LED_TYPE, DATA_PIN1, RGB> (C, LEDS_PER_PIN, 40);
-      // https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples
-      // FastLED.addLeds<LED_TYPE, DATA_PIN2, RGB> (C, 2*LEDS_PER_PIN, LEDS_PER_PIN);
+  FastLED.addLeds<LED_TYPE, DATA_PIN0, RGB> (C, 0, LEDS_PER_PIN);
+  int edges[MAX_CRYSTALS_NUM][MAX_EDGE_PER_CRYSTAL + 1] = {{-1}, {7, 4, 2, 18, 15, 3, 16, 6, 5, -1}, {8, 9, 7, 6, 4, 3, 1, -1}, {11, 13, 12, 4, 2, 18, 16, 15, 1, -1}, {10, 12, 11, 9, 3, 2, 1, -1}, {14, 16, 6, 1, -1}, {7, 2, 16, 5, 1, -1}, {9, 8, 6, 2, 1, -1}, {9, 7, 2, -1}, {11, 10, 8, 7, 4, 2, -1}, {11, 9, 4, -1}, {10, 9, 12, 4, 3, -1}, {11, 4, 18, 13, 3, -1}, {17, 18, 12, 3, -1}, {16, 5, -1}, {18, 16, 3, 1, -1}, {14, 18, 15, 3, 6, 5, 1, -1}, {18, 13, -1}, {17, 13, 12, 16, 15, 3, 1, -1}};
+  g = new Graph(edges,MAX_CRYSTALS_NUM);
+  Serial.begin(9600);
+  FastLED.addLeds<LED_TYPE, DATA_PIN1, RGB> (C, LEDS_PER_PIN, 40);
+  // https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples
+  // FastLED.addLeds<LED_TYPE, DATA_PIN2, RGB> (C, 2*LEDS_PER_PIN, LEDS_PER_PIN);
+  dist = g->calcDist(lookup[1]);
 }
 
 void loop() {
 
-  if(j == 17) {
-    j = 1;
-  }
-  int *dist;
-  int i, red, green;
-  dist = g->calcDist(lookup[j]);
-  
+  int i;
+  int red=0, green=0; 
   for(i = 1; i < 17; i++) {
-    
-    red = dist[lookup[i]]*85+k;
-    green =  255-(dist[lookup[i]]*85)+k;
-    if(red >255 || green > 250) {
-      red = green = 0;
-    }
-    CrystalColour(i, red, 0,green);
-    Serial.print(lookup[i]);
-    Serial.println(dist[lookup[i]]);
+    red = dist[lookup[i]]*85;
+    green =  255-(dist[lookup[i]]*85);
+    CrystalColour(i, red, blue,green);
+//    Serial.print(lookup[i]);
+//    Serial.println(dist[lookup[i]]);
+      Serial.println(blue);
   }
-  if(k != 250) {
-    k++;
-  } else {
-    k = 0;
-  }
-  delay(100);
+  delay(30);
 //  delete dist;
+
+  if(blue >= 40) {
+    blueFlag = 1;
+  } else if(blue == 0) {
+    blueFlag = 0;
+  }
+
+  if(blueFlag == 0) {
+    blue++;
+  } else {
+    blue--;
+  }
   FastLED.show();
 }
 
