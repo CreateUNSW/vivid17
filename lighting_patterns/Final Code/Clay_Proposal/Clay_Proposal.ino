addc7798e74745b7f57615a4b0edbfe6808d8b01
RandyTjang <randy.tjang@gmail.com>
2017-05-21 00:54:19 +1000

    Itegrated clay's proposal code

---
+++ lighting_patterns/Final Code/Clay_Proposal/Clay_Proposal.ino
@@ -0,0 +1,240 @@
+#include <EEPROM.h>     // We are going to read and write PICC's UIDs from/to EEPROM
+#include <SPI.h>        // RC522 Module uses SPI protocol
+#include <MFRC522.h>  // Library for Mifare RC522 Devices
+
+#define COMMON_ANODE
+
+#ifdef COMMON_ANODE
+#define LED_ON LOW
+#define LED_OFF HIGH
+#else
+#define LED_ON HIGH
+#define LED_OFF LOW
+#endif
+
+
+#define relay 4     // Set Relay Pin
+#define wipeB 3     // Button pin for WipeMode
+
+boolean match = false;          // initialize card match to false
+boolean proposeMode = false;  // initialize programming mode to false
+boolean replaceMaster = false;
+
+uint8_t successRead;    // Variable integer to keep if we have Successful Read from Reader
+
+byte storedCard[4];   // Stores an ID read from EEPROM
+byte readCard[4];   // Stores scanned ID read from RFID Module
+byte masterCard[4];   // Stores master card's ID read from EEPROM
+
+// Create MFRC522 instance.
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+///////////////////////////////////////// Setup ///////////////////////////////////
+void setup() {
+  //Arduino Pin Configuration
+  pinMode(wipeB, INPUT_PULLUP);   // Enable pin's pull up resistor
+  pinMode(relay, OUTPUT);
+  //Be careful how relay circuit behave on while resetting or power-cycling your Arduino
+  digitalWrite(relay, HIGH);    // Make sure door is locked
+
+  //Protocol Configuration
+  Serial.begin(9600);  // Initialize serial communications with PC
+  SPI.begin();           // MFRC522 Hardware uses SPI protocol
+  mfrc522.PCD_Init();    // Initialize MFRC522 Hardware
+
+  //If you set Antenna Gain to Max it will increase reading distance
+  mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+
+  Serial.println(F("Access Control Example v0.1"));   // For debugging purposes
+  ShowReaderDetails();  // Show details of PCD - MFRC522 Card Reader details
+
+  //Wipe Code - If the Button (wipeB) Pressed while setup run (powered on) it wipes EEPROM
+  if (digitalRead(wipeB) == LOW) {  // when button pressed pin should get low, button connected to ground
+    Serial.println(F("Wipe Button Pressed"));
+    Serial.println(F("You have 10 seconds to Cancel"));
+    Serial.println(F("This will be remove all records and cannot be undone"));
+    bool buttonState = monitorWipeButton(10000); // Give user enough time to cancel operation
+    if (buttonState == true && digitalRead(wipeB) == LOW) {    // If button still be pressed, wipe EEPROM
+      Serial.println(F("Starting Wiping EEPROM"));
+      for (uint16_t x = 0; x < EEPROM.length(); x = x + 1) {    //Loop end of EEPROM address
+        if (EEPROM.read(x) == 0) {              //If EEPROM address 0
+          // do nothing, already clear, go to the next address in order to save time and reduce writes to EEPROM
+        }
+        else {
+          EEPROM.write(x, 0);       // if not write 0 to clear, it takes 3.3mS
+        }
+      }
+      Serial.println(F("EEPROM Successfully Wiped"));
+    }
+    else {
+      Serial.println(F("Wiping Cancelled")); // Show some feedback that the wipe button did not pressed for 15 seconds
+    }
+  }
+  // Check if master card defined, if not let user choose a master card
+  // This also useful to just redefine the Master Card
+  // You can keep other EEPROM records just write other than 143 to EEPROM address 1
+  // EEPROM address 1 should hold magical number which is '143'
+  if (EEPROM.read(1) != 143) {
+    Serial.println(F("No Master Card Defined"));
+    Serial.println(F("Scan A PICC to Define as Master Card"));
+    do {
+      successRead = getID();            // sets successRead to 1 when we get read from reader otherwise 0
+    }
+    while (!successRead);                  // Program will not go further while you not get a successful read
+    for ( uint8_t j = 0; j < 4; j++ ) {        // Loop 4 times
+      EEPROM.write( 2 + j, readCard[j] );  // Write scanned PICC's UID to EEPROM, start from address 3
+    }
+    EEPROM.write(1, 143);                  // Write to EEPROM we defined Master Card.
+    Serial.println(F("Master Card Defined"));
+  }
+  Serial.println(F("-------------------"));
+  Serial.println(F("Master Card's UID"));
+  for ( uint8_t i = 0; i < 4; i++ ) {          // Read Master Card's UID from EEPROM
+    masterCard[i] = EEPROM.read(2 + i);    // Write it to masterCard
+    Serial.print(masterCard[i], HEX);
+  }
+  Serial.println("");
+  Serial.println(F("-------------------"));
+  Serial.println(F("Everything is ready"));
+  Serial.println(F("Waiting PICCs to be scanned"));
+  //cycleLeds();    // Everything ready lets give user some feedback by special patterns
+}
+
+
+///////////////////////////////////////// Main Loop ///////////////////////////////////
+void loop () {
+  do {
+    successRead = getID();  // sets successRead to 1 when we get read from reader otherwise 0
+
+    if (proposeMode) {
+      ProposeLeds();              // Propose Mode run propose pattern
+    }
+    else {
+      normalModeOn();     // Normal mode, Vivid Sydney patterns
+    }
+  }
+  while (!successRead);   //the program will not go further while you are not getting a successful read
+  if (proposeMode) {
+    if ( isMaster(readCard) ) { //When in propose mode check First If master card scanned again to exit program mode, and change to Vivid mode
+      Serial.println(F("Master Card Scanned"));
+      Serial.println(F("Exiting Propose Mode"));
+      Serial.println(F("-----------------------------"));
+      proposeMode = false;
+      return;
+    }
+    else {
+      //************************************//
+      //Insert propose pattern code or Run propose sub_function
+      //************************************//
+      }
+    }
+  else {
+    if ( isMaster(readCard)) {    // If scanned card's ID matches Master Card's ID - enter propose mode
+      proposeMode = true;
+      Serial.println(F("Hello Master - Entered Propose Mode"));
+    }
+    else{
+      //************************************//
+      //Insert Vivid pattern code or Run Vivid sub_function
+      //************************************//
+      }
+    }
+}
+
+
+///////////////////////////////////////// Get PICC's UID ///////////////////////////////////
+uint8_t getID() {
+  // Getting ready for Reading PICCs
+  if ( ! mfrc522.PICC_IsNewCardPresent()) { //If a new PICC placed to RFID reader continue
+    return 0;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {   //Since a PICC placed get Serial and continue
+    return 0;
+  }
+  // There are Mifare PICCs which have 4 byte or 7 byte UID care if you use 7 byte PICC
+  // I think we should assume every PICC as they have 4 byte UID
+  // Until we support 7 byte PICCs
+  Serial.println(F("Scanned PICC's UID:"));
+  for ( uint8_t i = 0; i < 4; i++) {  //
+    readCard[i] = mfrc522.uid.uidByte[i];
+    Serial.print(readCard[i], HEX);
+  }
+  Serial.println("");
+  mfrc522.PICC_HaltA(); // Stop reading
+  return 1;
+}
+
+void ShowReaderDetails() {
+  // Get the MFRC522 software version
+  byte v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
+  Serial.print(F("MFRC522 Software Version: 0x"));
+  Serial.print(v, HEX);
+  if (v == 0x91)
+    Serial.print(F(" = v1.0"));
+  else if (v == 0x92)
+    Serial.print(F(" = v2.0"));
+  else
+    Serial.print(F(" (unknown),probably a chinese clone?"));
+  Serial.println("");
+  // When 0x00 or 0xFF is returned, communication probably failed
+  if ((v == 0x00) || (v == 0xFF)) {
+    Serial.println(F("WARNING: Communication failure, is the MFRC522 properly connected?"));
+    Serial.println(F("SYSTEM HALTED: Check connections."));
+    while (true); // do not go further
+  }
+}
+
+///////////////////////////////////////// Propose Leds (Propose Mode) ///////////////////////////////////
+void ProposeLeds() {
+//**********************************************************************
+//Insert propose pattern code//
+//**********************************************************************//
+}
+
+//////////////////////////////////////// Normal Mode Led  ///////////////////////////////////
+void normalModeOn () {
+//**********************************************************************//
+//Insert propose pattern code//
+//**********************************************************************//
+}
+
+
+///////////////////////////////////////// Check Bytes   ///////////////////////////////////
+boolean checkTwo ( byte a[], byte b[] ) {
+  if ( a[0] != 0 )      // Make sure there is something in the array first
+    match = true;       // Assume they match at first
+  for ( uint8_t k = 0; k < 4; k++ ) {   // Loop 4 times
+    if ( a[k] != b[k] )     // IF a != b then set match = false, one fails, all fail
+      match = false;
+  }
+  if ( match ) {      // Check to see if if match is still true
+    return true;      // Return true
+  }
+  else  {
+    return false;       // Return false
+  }
+}
+
+
+////////////////////// Check readCard IF is masterCard   ///////////////////////////////////
+// Check to see if the ID passed is the master programing card
+boolean isMaster( byte test[] ) {
+  if ( checkTwo( test, masterCard ) )
+    return true;
+  else
+    return false;
+}
+
+bool monitorWipeButton(uint32_t interval) {
+  uint32_t now = (uint32_t)millis();
+  while ((uint32_t)millis() - now < interval)  {
+    // check on every half a second
+    if (((uint32_t)millis() % 500) == 0) {
+      if (digitalRead(wipeB) != LOW)
+        return false;
+    }
+  }
+  return true;
+}
