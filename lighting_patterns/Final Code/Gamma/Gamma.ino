20d7dbc0f873c4df3e688875da98610ba7cc28ff
wilzlerr <william.weng@hotmail.com>
2017-05-23 01:47:47 +1000

    fixed gamma

--- lighting_patterns/Final Code/Gamma/Gamma.ino
+++ lighting_patterns/Final Code/Gamma/Gamma.ino
@@ -108,9 +108,11 @@ uint8_t maxDistance = 1;
 // Main loop global variables
 uint8_t forceChange = 0;
 boolean transition = true;
-int patternTemp = 0;
+int wallTemp = 0;
+int wingTemp = 0;
 int transitionTemp = 0;
-int choosePattern = 1;
+int chooseWall = 1;
+int chooseWing = 1;
 int chooseTransition = 1;
 
 // ====================
@@ -127,11 +129,6 @@ uint8_t radialIndex = 0;
 
 // ============ SETUP ============ SETUP ============ SETUP ============ SETUP ============ SETUP ============
 void setup() {
-  
-  Serial.begin(9600);  // Initialize serial communications with PC
-
-  setupRFID();
-  
   srand(0);
   FastLED.addLeds<LED_TYPE, RIGHT_GREEN, RGB>(leds, RG_INDEX, 172);
   FastLED.addLeds<LED_TYPE, RIGHT_YELLOW, RGB>(leds, RY_INDEX, 180);
@@ -150,12 +147,8 @@ void setup() {
 }
 // ============ // ============  // ============  // ============  // ============  // ============ 
 void loop() {
+
 //================================================= PATTERN CODE GOES HERE ================================================= 
-  // Calls a function that checks for Clay's card and tries to enter his proposal loop,
-  // during which the card is continually checked for again. If detected again, it will exit 
-  // internal loop, and out of the function call, resuming vivid code
-  doProposal();
-  
   int centre = 259;
   // Note sensors are active low
   if(!digitalRead(sensorPins[4]) && !digitalRead(sensorPins[3]) && !digitalRead(sensorPins[2]) && !digitalRead(sensorPins[1])) {
@@ -183,11 +176,13 @@ void loop() {
   // Changes the pattern after 25 seconds when the scene changes
   if(t == 0) {
     forceChange++;
-    patternTemp = rand();
+    wallTemp = rand();
+    wingTemp = rand();
     transitionTemp = rand();
     if(forceChange == 10) {
       forceChange = 0;
-      choosePattern = patternTemp;
+      chooseWall = wallTemp;
+      chooseWing = wingTemp;
       chooseTransition = transitionTemp;
     }
   }
@@ -198,14 +193,15 @@ void loop() {
     prevWing = currWing;
     fadeSpeed = FADE_AMOUNT;
     radialIndex = 0;
-    choosePattern = patternTemp;
+    chooseWall = wallTemp;
+    chooseWing = wingTemp;
     chooseTransition = transitionTemp;
     change = true;
   }
   
   // Choosing pattern
   if(currWing == NULL) {
-    switch (choosePattern % 6) {
+    switch (chooseWall % 6) {
       case 0 :
         shimmerCenter(currWing, centre);
         break;
@@ -215,6 +211,7 @@ void loop() {
       case 2 :
         if(fadeSpeed < 1 + 0.1) fadeSpeed = 0.98;
         shimmerCenter(currWing, 259);
+        chooseTransition = 0;
         break;
       case 3 :
         crystalGradient(currWing);
@@ -229,7 +226,7 @@ void loop() {
         shimmerCenter(currWing, 259);
     }
   } else {
-    switch (choosePattern % 8) {
+    switch (chooseWing % 8) {
       case 0 :
         if(fadeSpeed < 1 + 0.1) fadeSpeed = 0.98;
         shimmerCenter(currWing, centre);
@@ -430,7 +427,7 @@ void chrisWings() {
       blue = chris[index*3+2];
       crystalRGB(index, red, green, blue);
   }
-  patternTemp = rand();
+  chooseWing = rand();
 }
 
 // Clay marriage pattern
